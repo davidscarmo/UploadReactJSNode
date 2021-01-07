698f4a5a3f64759b9ea942304a7fa147966e881a
David Carmo <davidsc2812@gmail.com>
2021-01-07 09:48:16 -0300

    Iniciando frontend
    foi iniciado o frontend da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'; 
+
+
+class App extends Component {
+  render()
+  {
+    return <div className="App">
+      hello my brudha
+    </div>
+  }
+}
+
+export default App;
